8ea7a1ff8e1af77771d4dffcae4bc4ef20daa76f
stopopol <christoph.wohner@umweltbundesamt.at>
2022-03-15 11:34:45 +0100

    Update deims_properties_formatter.js

--- js/deims_properties_formatter.js
+++ js/deims_properties_formatter.js
@@ -3,13 +3,13 @@ Drupal.behaviors.deims_properties_formatter = {
 
 	var input_data = drupalSettings.deims_properties_formatter.data_object;	
 	
-	data = [{
+	var data = [{
 		type: "treemap",
 		ids: input_data['ids'],
 		labels: input_data['labels'],
 		parents: input_data['parents'],
 		marker: {colors: input_data['colors']}
-	}]
+	}];
 	
 	var layout = {
 		margin: {
@@ -21,7 +21,9 @@ Drupal.behaviors.deims_properties_formatter = {
 		},
 	};
 	
-	Plotly.newPlot('properties_div', data, layout)
+	var config = {responsive: true}; 
+	  
+	Plotly.newPlot('properties_div', data, layout, config);
 	
   }
   
